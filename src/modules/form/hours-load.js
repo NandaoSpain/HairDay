74edd39fc196c6207f2aa3a51a49da877ff3c2b5
Fernando Rodrigues <117524258+NandaoSpain@users.noreply.github.com>
2024-11-19 19:01:31 -0300

    list date

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -1,9 +1,11 @@
 import dayjs from "dayjs"
 import { openingHours } from "../../utils/opening-hours.js"
+import { hoursClick } from "./hours-click.js"
 
 const hours = document.getElementById("hours")
 
 export function hoursLoad({ date }) {
+  hours.innerHTML = ""
   const opening = openingHours.map((hour) =>{
     const [scheduleHour] = hour.split(':')
 
@@ -20,7 +22,24 @@ export function hoursLoad({ date }) {
     li.classList.add(available ? "hour-available" : "hour-unavailable")
     li.textContent = hour
 
+    if(hour === "9:00") {
+      hourHeaderAdd('Manhã')
+    } else if(hour === "13:00") {
+      hourHeaderAdd('Tarde')
+    } else if(hour === "18:00") {
+      hourHeaderAdd('Noite')
+    }
+
     hours.appendChild(li)
    
   })
+
+  hoursClick()
+}
+
+function hourHeaderAdd(title) {
+  const header = document.createElement("li")
+  header.classList.add("hour-period")
+  header.textContent = title
+  hours.append(header)
 }
